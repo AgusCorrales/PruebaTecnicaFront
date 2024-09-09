a6a3546a0dfbaa3692c08b706676a8e681184f71
AgusCorrales <aguscorrales1997@gmail.com>
2024-09-09 09:41:47 +0200

    update

--- src/app/components/news/news.component.ts
+++ src/app/components/news/news.component.ts
@@ -15,17 +15,17 @@ import { FormsModule } from '@angular/forms';
 export class NewsComponent implements OnInit {
 
   public news: Array<NewsInterface> = [];
-    public newNews: NewsInterface = {
-      title: "",
-      description: "",
-      date: "",
-      content: "",
-      auth: "",
+  public newNews: NewsInterface = {
+    title: "",
+    description: "",
+    date: "",
+    content: "",
+    auth: "",
 
-    };
+  };
 
-   public showForm: boolean = false;
-   public isLiked: boolean = false;
+  public showForm: boolean = false;
+  public isLiked: boolean = false;
 
   constructor(private newsService: NewsService) { }
 
@@ -37,13 +37,13 @@ export class NewsComponent implements OnInit {
     this.newsService.getNews().subscribe({
       next: (data: NewsInterface[]) => {
         this.news = data
-        .filter(newsItem => (newsItem.likes ?? []).length === 0)
-        .sort((a, b) => {
-          const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
-          const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
-          
-          return dateB - dateA;
-        });
+          .filter(newsItem => (newsItem.likes ?? []).length === 0)
+          .sort((a, b) => {
+            const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
+            const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
+
+            return dateB - dateA;
+          });
 
       },
       error: (error) => {
@@ -60,7 +60,7 @@ export class NewsComponent implements OnInit {
 
           const dateA = a.createdAt ? new Date(a.createdAt).getTime() : 0;
           const dateB = b.createdAt ? new Date(b.createdAt).getTime() : 0;
-  
+
           return dateB - dateA;
         });
         this.newNews = { title: '', description: '', content: '', date: "", auth: '' };
@@ -86,47 +86,49 @@ export class NewsComponent implements OnInit {
 
   }
 
-   likeNews(newsId: string) {
-      this.newsService.likeNews(newsId).subscribe({
-        next: () => {
-          const index = this.news.findIndex(news => news._id === newsId);
-          if (index !== -1) {
-            this.getNews(); 
-          }
-        },
-        error: (error) => {
-          console.error('Error al dar like:', error);
-        }
-      });
-    
-  }
-
-  notLikeNews(newsId: string) {
-    this.newsService.notLikeNews(newsId).subscribe({
-      next: () => {      
+  likeNews(newsId: string) {
+    this.newsService.likeNews(newsId).subscribe({
+      next: () => {
         const index = this.news.findIndex(news => news._id === newsId);
         if (index !== -1) {
-          this.getNews(); 
+          this.getNews();
         }
       },
       error: (error) => {
         console.error('Error al dar like:', error);
       }
     });
+
+  }
+
+  notLikeNews(newsId: string) {
+    this.newsService.notLikeNews(newsId).subscribe({
+      next: () => {
+        const index = this.news.findIndex(news => news._id === newsId);
+        if (index !== -1) {
+          this.getNews();
+        }
+      },
+      error: (error) => {
+        console.error('Error al dar like:', error);
+      }
+    });
+
+  }
+
   
-}
+  toggleForm() {
+    this.showForm = !this.showForm;
+  }
 
-   toggleForm() {
-     this.showForm = !this.showForm;
-   }
 
-   toggleLike(newsId: string) {
-    this.isLiked = !this.isLiked; 
+  toggleLike(newsId: string) {
+    this.isLiked = !this.isLiked;
 
     if (this.isLiked) {
-      this.likeNews(newsId); 
+      this.likeNews(newsId);
     } else {
-      this.notLikeNews(newsId); 
+      this.notLikeNews(newsId);
     }
   }
 
